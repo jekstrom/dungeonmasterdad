0b071205adb60a8a1d109313ebb5319a870db127
James Ekstrom <james@jamesekstrom.com>
2026-02-09 19:26:19 -0800

    add paper factory placeholder

---
+++ buildings/buildables/paper_factory.gd
@@ -0,0 +1,8 @@
+class_name PaperFactory extends Building
+
+func _process(delta: float) -> void:
+	if is_ghost: return
+	timer += delta
+	if timer >= interval:
+		timer -= interval
+		PlayerManager.update_reality_level(1)
